42be29539b7d147afb4407621d438e90029ba922
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2018-04-10 20:22:06 +0800

    Buat Method CetakInfo()

--- src/praktek03/Mobil.java
+++ src/praktek03/Mobil.java
@@ -4,5 +4,12 @@ public class Mobil {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk  \t: "+merk+"\n"+
+                "Warna\t:" +warna+" \n"+
+                "Harga\t: "+harga);
+        
+    }
   
 }
