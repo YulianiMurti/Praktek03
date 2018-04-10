cfe63860df20372d3ffde00942e2d8df610b11b8
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2018-04-10 21:30:15 +0800

    Atribute Dan Method

--- src/praktek03/MobilClasic.java
+++ src/praktek03/MobilClasic.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class MobilClasic extends Mobil{
+    private String jenisClasic;
+    
+    void throttle(){
+        System.out.printeln("Kunoooo...............");
+    }
     
 }
