d0f48e85f912b68a284b3697d81d2ef456657521
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2018-04-10 20:53:40 +0800

    Instance, buat atribut dan panggil method

--- src/praktek03/MobilAksi.java
+++ src/praktek03/MobilAksi.java
@@ -2,7 +2,12 @@ package praktek03;
 
 public class MobilAksi {
     public static void main(String[] args) {
+        Mobil s = new Mobil();
+        s.merk="Honda";
+        s.warna="Biru";
+        s.harga=1900;
         
+        s.cetakInfo();
     }
     
 }
