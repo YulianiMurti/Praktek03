046c4129cd6c00de273c4665763400c892a900b5
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2018-04-10 21:19:21 +0800

    Menggunakan Method Get

--- src/praktek03/MobilAksi.java
+++ src/praktek03/MobilAksi.java
@@ -9,6 +9,14 @@ public class MobilAksi {
         s.setHarga(1900);
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t:");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t:");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t:");
+        System.out.println(s.getHarga());
     }
     
+
 }
